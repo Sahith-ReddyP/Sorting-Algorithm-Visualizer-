da991be402c629d7368b544415456df6a4aab9a3
Sahithi Reddy <72133374+Sahith-ReddyP@users.noreply.github.com>
2022-07-10 08:12:50 +0530

    Update insertion_sort.js

--- scripts/insertion_sort.js
+++ scripts/insertion_sort.js
@@ -1,9 +1,4 @@
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    E-MAIL:-    tummala.kethan@gmail.com
-    *****************
-*/
+
 
 function Insertion()
 {
@@ -48,9 +43,4 @@ function Insertion()
     enable_buttons();
 }
 
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    E-MAIL:-    tummala.kethan@gmail.com
-    *****************
-*/
+
